cfa8b41c3e366d6322e549adea6606e84d80afcd
B Ricks <bricks@unomaha.edu>
2020-05-05 20:59:07 -0500

    Add multiple letters

--- game.js
+++ game.js
@@ -25,6 +25,10 @@ let Scenes = {
   ]
 }
 
+let scale = .05;
+let startY = 0;
+let stopY = 20;
+
 class BallBehavior extends Base.Behavior {
   start() {
     this.time = 0;
@@ -33,7 +37,19 @@ class BallBehavior extends Base.Behavior {
     this.time += .1;
     this.gameObject.y = -this.time;
     this.gameObject.x = Math.sin(this.time/2) * .5;
-    if (this.gameObject.y < -20)
+    if(this.time < 1){
+      this.gameObject.scaleX = this.time * scale;
+      this.gameObject.scaleY = this.time * scale;
+    }
+    else if (this.time > stopY - 1){
+      this.gameObject.scaleX = (stopY - this.time) * scale;
+      this.gameObject.scaleY = (stopY - this.time) * scale;
+    }
+    else{
+      this.gameObject.scaleX = scale;
+      this.gameObject.scaleY = scale;
+    }
+    if (this.gameObject.y < -stopY)
       this.end();
   }
   end() {
@@ -46,6 +62,8 @@ class BallBehavior extends Base.Behavior {
 
 class SpawnerBehavior extends Base.Behavior {
   start() {
+    this.nextLetter = 0;
+    this.letters = ["p", "m", "x"]
     this.ticksSinceLastSpawn = 100;
   }
   update() {
@@ -56,13 +74,15 @@ class SpawnerBehavior extends Base.Behavior {
     }
   }
   spawn() {
-    let object = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Circle, new Base.Point(0, 0), new Base.Point(.05, .05))
+    let object = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Circle, new Base.Point(0, startY), new Base.Point(0, 0))
+    object.getComponent("CircleComponent").fill = "orange"
     let behavior = new BallBehavior();
     object.addComponent(behavior);
     behavior.start();
-    let letter = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Text, new Base.Point(0, 0), new Base.Point(2, 2), 0, object);
+    let letter = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Text, new Base.Point(-12, 8), new Base.Point(2, 2), 0, object);
     let textComponent = letter.getComponent("TextComponent");
-    textComponent.text = "p";
+    textComponent.text = this.letters[(this.nextLetter++)%this.letters.length];
+    
   }
 }
 
