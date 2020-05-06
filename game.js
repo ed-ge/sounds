1914e679381d349b1cd3f83ff93357d761dbf186
B Ricks <bricks@unomaha.edu>
2020-05-05 20:50:36 -0500

    Add horizontal motion

--- game.js
+++ game.js
@@ -27,56 +27,49 @@ let Scenes = {
 
 class BallBehavior extends Base.Behavior {
   start() {
-
+    this.time = 0;
   }
   update() {
-    this.gameObject.y -= .1;
-
-
-
+    this.time += .1;
+    this.gameObject.y = -this.time;
+    this.gameObject.x = Math.sin(this.time/2) * .5;
+    if (this.gameObject.y < -20)
+      this.end();
+  }
+  end() {
+    Base.SceneManager.currentScene.destroy(this.gameObject);
   }
   onTouchEnd() {
-    console.log("Touch end")
-    Base.SceneManager.currentScene.destroy(this.gameObject);
-    this.gameObject.y += 10;
-    // this.gameObject.x += (Math.random() * 2 - 1) * this.scale;
-    // this.gameObject.y += (Math.random() * 2 - 1) * this.scale;
-
-    // this.gameObject.x = Math.max(-10, Math.min(this.gameObject.x, 10));
-    // this.gameObject.y = Math.max(-10, Math.min(this.gameObject.y, 10));
+    this.end();
   }
 }
 
 class SpawnerBehavior extends Base.Behavior {
   start() {
     this.ticksSinceLastSpawn = 100;
-    
   }
   update() {
     this.ticksSinceLastSpawn++;
-    if(this.ticksSinceLastSpawn >= 100)
-    {
+    if (this.ticksSinceLastSpawn >= 100) {
       this.ticksSinceLastSpawn -= 100;
       this.spawn();
     }
-    
-
   }
-  spawn(){
-    let object = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Circle, new Base.Point(0,0), new Base.Point(.05, .05))
-    object.addComponent(new BallBehavior())
-    let letter = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Text, new Base.Point(0,0), new Base.Point(2,2), 0, object);
+  spawn() {
+    let object = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Circle, new Base.Point(0, 0), new Base.Point(.05, .05))
+    let behavior = new BallBehavior();
+    object.addComponent(behavior);
+    behavior.start();
+    let letter = Base.SceneManager.currentScene.instantiate(Base.Prefabs.Text, new Base.Point(0, 0), new Base.Point(2, 2), 0, object);
     let textComponent = letter.getComponent("TextComponent");
     textComponent.text = "p";
   }
-
 }
 
 
 let GameBehaviors = {
-
   BallBehavior,
-  SpawnerBehavior, 
+  SpawnerBehavior,
 }
 
 Base.main({}, GameBehaviors, Scenes);
